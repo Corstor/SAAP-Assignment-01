aefc6fd6a3b5f3cb8509a5a79730d345f192c349
Corrado Stortini <corrado.stortini2@studio.unibo.it>
2024-10-07 17:28:12 +0200

    Add userImpl

--- src/main/java/layered/persistence/EBike.java
+++ src/main/java/layered/persistence/EBike.java
@@ -9,11 +9,11 @@ public interface EBike {
     P2d getPosition();
     
     void rechargeBattery();
-    void decreaseBattery(final int delta);
+    void decreaseBattery(int delta);
     boolean isAvailable();
 
-    void updateState(final EBikeState state);
-    void updateLocation(final P2d location);
-    void updateSpeed(final double speed);
-    void updateDirection(final V2d direction);
+    void updateState(EBikeState state);
+    void updateLocation(P2d location);
+    void updateSpeed(double speed);
+    void updateDirection(V2d direction);
 }
---
+++ src/main/java/layered/persistence/UserImpl.java
@@ -0,0 +1,36 @@
+package layered.persistence;
+
+public class UserImpl implements User {
+
+    private final String id;
+    private int credit;
+
+    public UserImpl(final String id) {
+        this.id = id;
+        this.credit = 0;
+    }
+
+    @Override
+    public String getId() {
+        return this.id;
+    }
+
+    @Override
+    public int getCredit() {
+        return this.credit;
+    }
+
+    @Override
+    public void decreaseCredit(final int amount) {
+        this.credit -= amount;
+    }
+
+    @Override
+    public void rechargeCredit(final int amount) {
+        this.credit += amount;
+    }
+
+    public String toString() {
+		return "{ id: " + this.id + ", credit: " + this.credit + " }";
+	}
+}
