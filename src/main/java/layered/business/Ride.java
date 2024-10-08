a06491c2cecd9c451ad546113be169d30a196e19
Corrado Stortini <corrado.stortini2@studio.unibo.it>
2024-10-08 12:38:41 +0200

    Add ride interface

--- src/main/java/layered/business/Ride.java
+++ src/main/java/layered/business/Ride.java
@@ -1,5 +1,25 @@
 package layered.business;
 
+import java.util.Date;
+import java.util.Optional;
+
+import layered.persistence.ebike.EBike;
+import layered.persistence.user.User;
+
 public interface Ride {
-    
+    String getId();
+
+    void start();
+
+    void end();
+
+    Date getStartedDate();
+
+    boolean isOngoing();
+
+    Optional<Date> getEndDate();
+
+    User getUser();
+
+    EBike getEBike();
 }
--- src/main/java/layered/business/RideImpl.java
+++ src/main/java/layered/business/RideImpl.java
@@ -1,5 +1,59 @@
 package layered.business;
 
+import java.util.Date;
+import java.util.Optional;
+
+import layered.persistence.ebike.EBike;
+import layered.persistence.user.User;
+
 public class RideImpl implements Ride {
+
+    @Override
+    public String getId() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getId'");
+    }
+
+    @Override
+    public void start() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'start'");
+    }
+
+    @Override
+    public void end() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'end'");
+    }
+
+    @Override
+    public Date getStartedDate() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getStartedDate'");
+    }
+
+    @Override
+    public boolean isOngoing() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isOngoing'");
+    }
+
+    @Override
+    public Optional<Date> getEndDate() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getEndDate'");
+    }
+
+    @Override
+    public User getUser() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getUser'");
+    }
+
+    @Override
+    public EBike getEBike() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getEBike'");
+    }
     
 }
