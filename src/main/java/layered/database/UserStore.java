b35dfa5158868da7bae9855bd2295ae4070ad990
Corrado Stortini <corrado.stortini2@studio.unibo.it>
2024-10-14 12:39:48 +0200

    Add some more functions

--- src/main/java/layered/database/UserStore.java
+++ src/main/java/layered/database/UserStore.java
@@ -9,6 +9,8 @@ public interface UserStore {
     void saveAllUsers(List<UserDB> users) throws IOException;
 
     void saveUser(UserDB user) throws IOException;
+    void deleteUser(String id) throws IOException;
+
     void changeUserCredit(String id, int credit) throws IOException;
     UserDB getUserFromId(String id) throws IOException;
 }
--- src/main/java/layered/database/UserStoreImpl.java
+++ src/main/java/layered/database/UserStoreImpl.java
@@ -7,11 +7,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.core.type.TypeReference;
 import java.io.File;
 import java.io.IOException;
-import java.util.Optional;
 
 public class UserStoreImpl implements UserStore {
     private final File jsonFile;
-    private final List<UserDB> users;
+    private List<UserDB> users;
     private final ObjectMapper objectMapper;
 
     public UserStoreImpl() throws IOException {
@@ -20,33 +19,6 @@ public class UserStoreImpl implements UserStore {
         this.users = new LinkedList<>();
 
         this.saveAllUsers(this.users); //Create the file
-    }
-
-    @Override
-    public void saveUser(UserDB user) throws IOException {
-        this.users.add(user);
-        this.saveAllUsers(this.users);
-    }
-
-    @Override
-    public void changeUserCredit(String id, int credit) throws IOException {
-        Optional<UserDB> userOpt = users.stream()
-        .filter(user -> user.id().equals(id))
-        .findFirst();
-
-        if (userOpt.isPresent()) {
-            UserDB user = userOpt.get();
-            this.users.remove(user);
-            this.users.add(new UserDB(user.id(), credit));
-            this.saveAllUsers(this.users);
-        }
-    }
-
-    @Override
-    public UserDB getUserFromId(String id) {
-        return users.stream()
-            .filter(user -> user.id().equals(id))
-            .findFirst().get();
     }
 
     @Override
@@ -61,5 +33,30 @@ public class UserStoreImpl implements UserStore {
     public void saveAllUsers(List<UserDB> users) throws IOException {
         objectMapper.writerWithDefaultPrettyPrinter().writeValue(jsonFile, users);
     }
-    
+
+    @Override
+    public void saveUser(UserDB user) throws IOException {
+        this.users.add(user);
+        this.saveAllUsers(this.users);
+    }
+
+    @Override
+    public void deleteUser(String id) throws IOException {
+        this.users = this.users.stream().filter(user -> !user.id().equals(id)).toList();
+        this.saveAllUsers(this.users);
+    }
+
+    @Override
+    public void changeUserCredit(String id, int credit) throws IOException {
+        this.deleteUser(id);
+        this.users.add(new UserDB(id, credit));
+        this.saveAllUsers(this.users);
+    }
+
+    @Override
+    public UserDB getUserFromId(String id) {
+        return users.stream()
+            .filter(user -> user.id().equals(id))
+            .findFirst().get();
+    }
 }
